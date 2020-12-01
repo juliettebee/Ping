889df36ccd29d8df3854dd6d31ed6a90cf77fa4c
Juliette <git@juliette.page>
2020-12-01 17:57:08 -0500

    Switch to enable/disable tweak

--- Prefs/Prefs.h
+++ Prefs/Prefs.h
@@ -1,4 +1,5 @@
 #import <Preferences/PSListController.h>
+#import <Cephei/HBPreferences.h>	
 
 @interface JuliettePingPreferences : PSListController
 @end
--- Prefs/Prefs.m
+++ Prefs/Prefs.m
@@ -1,9 +1,35 @@
 #import "Prefs.h"
 
 @implementation JuliettePingPreferences
+    // Switch to enable/disable tweak
+    -(instancetype)init {
+        self = [super init];
+        
+        if (self) {
+            UISwitch *enableTweak = [[UISwitch alloc] init];
+            enableTweak.onTintColor = [UIColor colorWithRed: 0.99 green: 0.81 blue: 0.87 alpha: 1.00];
+            [enableTweak setOn:true animated:NO];
+            // Setup for response to switch update
+            [enableTweak addTarget:self action:@selector(tweakStatus:) forControlEvents:UIControlEventValueChanged];
+            UIBarButtonItem* switchAsAnUIBarButtonItemBecauseThatsANeededThingAndCausesCrashesIfItIsntLikeThis= [[UIBarButtonItem alloc] initWithCustomView: enableTweak];
+            self.navigationItem.rightBarButtonItem = switchAsAnUIBarButtonItemBecauseThatsANeededThingAndCausesCrashesIfItIsntLikeThis;
+        }
+        return self;
+    }
+
     - (NSArray *)specifiers {
         if (!_specifiers)
             _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
         return _specifiers;
+    }
+
+    -(void)tweakStatus:(id)sender {
+        BOOL state = [sender isOn];
+        // Disable tweak
+        HBPreferences *preferences  = [[HBPreferences alloc] initWithIdentifier:@"page.juliette.Ping.Prefs"];
+        [preferences registerDefaults:@{
+            @"Enabled": @(state),
+        }];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"page.juliette.Ping.Prefs/reloadprefs" object:nil];
     }
 @end
