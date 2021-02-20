f6a9eabf73a3537f8631ef5bd093a13f11ac4df3
Juliette <git@juliette.page>
2021-02-20 15:28:15 -0500

    Switch properly shows status

--- Prefs/Prefs.m
+++ Prefs/Prefs.m
@@ -12,7 +12,7 @@
             preferences  = [[HBPreferences alloc] initWithIdentifier:@"page.juliette.Ping.Prefs"];
             BOOL enabled;
             [preferences registerBool:&enabled default:YES forKey:@"Enabled"];
-            [enableTweak setOn:true animated:NO];
+            [enableTweak setOn:enabled animated:NO];
             // Setup for response to switch update
             [enableTweak addTarget:self action:@selector(tweakStatus:) forControlEvents:UIControlEventValueChanged];
             UIBarButtonItem* switchAsAnUIBarButtonItemBecauseThatsANeededThingAndCausesCrashesIfItIsntLikeThis= [[UIBarButtonItem alloc] initWithCustomView: enableTweak];
